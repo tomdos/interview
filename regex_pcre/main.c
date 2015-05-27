703069cd6eaffe44ce167dd6c08129eda3ef256b
tomdos <chimpy@seznam.cz>
2015-05-27 12:25:24 +0200

    regex parser accept escaping (%%), new string_concat fun

--- regex_pcre/main.c
+++ regex_pcre/main.c
@@ -7,13 +7,14 @@
 #include <pcre2.h>
 #include "re.h"
 
-#define		BUFSIZE						512
+#define		GENERAL_BUFSIZE		512
 #define		USE_PCRE2POSIX		1
 #define		USE_INPUT_STDIO		1
-#define		USE_VERBOSE				0
+#define		USE_VERBOSE				1
 
 //FIXME - * at least something (shoud be used + insted)
 #if USE_PCRE2POSIX
+ #define		PATTERN_ESCAPE	"%%"
  #define		PATTERN_WORD		"(.*)"
 //"([^[:space:]]*)"
  /* more then one space */
@@ -39,7 +40,7 @@ input_realloc_buf(char **buf, size_t size)
 	char *new_buf;
 
 	assert(size >= 0);
-	new_size = size + BUFSIZE;
+	new_size = size + GENERAL_BUFSIZE;
 
 	new_buf = realloc(*buf, new_size);
 	if (new_buf == NULL) {
@@ -59,12 +60,19 @@ input_realloc_buf(char **buf, size_t size)
 
 /*
  * Parse input token.
- * Use proper regex based on a token stored in start.
+ * Use proper regex based on a token stored in start arg.
  * Return length of input token and proper regex stored in rstring 
  * or -1 if error occures.
+ * 
+ * Expected input:
+ * %{N}
+ * %{NG}
+ * %{NSN}
+ * %%
+ * 
  */
 int 
-input_pattern_parser_token(const char *start, char *rstring, size_t rstring_len)
+input_pattern_parser_token(char *start, char *rstring, size_t rstring_len)
 {
 	uint8_t greedy;
 	uint8_t whitespace;
@@ -77,15 +85,33 @@ input_pattern_parser_token(const char *start, char *rstring, size_t rstring_len)
 	greedy = 0;
 	whitespace = 0;
 	p = start;
+	/* 
+	 * Begin 
+	 */
 	if (*p++ != '%') return -1;
+	/* 
+	 * Escaping - if we escape % by %% we need to copy just single % into final regex
+	 * and jump over %% in input string. Therefore 2 is returned although just signle
+	 * character is in rstring buffer.
+	 *
+	 */
+	if (*p == '%') {
+		ret = snprintf(rstring, rstring_len, PATTERN_ESCAPE);
+		assert(ret > 0 && ret < rstring_len);
+		return 2;
+	}
 	if (*p++ != '{') return -1;
 	
-	/* Identifier */
+	/* 
+	 * Token identifier 
+	 */
 	ret = sscanf(p, "%u%n", &id, &len); 
 	if (ret != 1) return -1;
 	p += len; // jump behind processed number
 	
-	/* Whitespace */
+	/* 
+	 * Whitespace 
+	 */
 	if (*p == 'S') { 
 		whitespace = 1;
 		p++;
@@ -94,13 +120,19 @@ input_pattern_parser_token(const char *start, char *rstring, size_t rstring_len)
 		p += len;
 	}
 	
-	/* Greedy */
+	/* 
+	 * Greedy 
+	 */
 	else if (*p == 'G') {
 		p++;
 		greedy = 1;
 	}
 	
+	/* 
+	 * End 
+	 */
 	if (*p++ != '}') return -1;
+	
 	
 	/* 
 	 * Return proper format of regex in rstring. 
@@ -124,28 +156,46 @@ input_pattern_parser_token(const char *start, char *rstring, size_t rstring_len)
 	return p - start; //return token len
 }
 
+
+char *
+string_concat(char *dest, size_t dest_len, size_t *dest_size, const char *src, size_t src_len)
+{
+	char *new_dest;
+	size_t new_size;
+	
+	new_dest = dest;
+	
+	/* Resize input buffer is necessary. */
+	if ((*dest_size - dest_len) < src_len) {
+		//resize
+		new_size = (GENERAL_BUFSIZE < src_len) ? GENERAL_BUFSIZE + src_len : GENERAL_BUFSIZE;
+		new_dest = (char *) realloc(dest, new_size);
+		assert(new_dest);
+		
+		*dest_size = new_size;
+	}
+	
+	/* Must always fit because of above test. */
+	strncat(new_dest, src, src_len);
+	
+	return new_dest;
+}
+
 /*
  * Parse input pattern (whole input pattern string) and return supported PCRE regex.
- *
- * Supported regex:
- *  (\S*) - word with no space
- *  ((\S*\s\S*){N} - exactly N spaces
- *  (.*) - greedy
- *
  */
 char *
-input_pattern_parser(const char *pattern)
+input_pattern_parser(char *pattern)
 {
 	char *regex;
-	char regex_token[BUFSIZE];
+	char regex_token[GENERAL_BUFSIZE];
 	size_t len;
 	size_t i, j;
 	int ret;
 	
 	//FIXME - realloc the buffer 
-	regex = malloc(BUFSIZE);
+	regex = (char *) malloc(GENERAL_BUFSIZE);
 	assert(regex);
-	
 	
 	//FIXME - check buffer len
 	i = 0;
@@ -154,7 +204,7 @@ input_pattern_parser(const char *pattern)
 	while (pattern[i]) {
 		//FIXME - escape
 		if (pattern[i] == '%') {
-			ret = input_pattern_parser_token(&pattern[i], regex_token, BUFSIZE);
+			ret = input_pattern_parser_token(&pattern[i], regex_token, GENERAL_BUFSIZE);
 			if (ret == -1)
 				return NULL;
 				
@@ -169,6 +219,7 @@ input_pattern_parser(const char *pattern)
 		}
 	}
 	regex[j] = '$';
+	//FIXME null??
 	
 	return regex;
 }
@@ -183,11 +234,11 @@ process_line(const char *line, const char *pattern)
 char *
 input_read()
 {
-	static char buf[BUFSIZE];
+	static char buf[GENERAL_BUFSIZE];
 	size_t len;
 
 	//FIXME - long line, input check
-	fgets(buf, BUFSIZE, stdin);
+	fgets(buf, GENERAL_BUFSIZE, stdin);
 	len = strlen(buf);
 	buf[len-1]='\0'; // remove nl
 
